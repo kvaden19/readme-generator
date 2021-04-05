d3ebcffda8bf264d318cfc38e12d13410053afcb
Kelly Vaden <klvaden@gmail.com>
2021-04-05 07:21:44 -0400

    Added starter code and package.json

---
+++ index.js
@@ -0,0 +1,39 @@
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Change to README prompts for user input
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      name: 'stack',
+      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    },
+    {
+      type: 'list',
+      message: 'What is your preferred method of communication?',
+      name: 'contact',
+      choices: ['email', 'phone', 'telekinesis'],
+    },
+  ])
+  // TODO: write to a .md file
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
